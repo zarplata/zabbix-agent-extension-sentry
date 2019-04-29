b884630c56c79528d0ea7576fa1a2da570106236
Andrey Kitsul <a.kitsul@office.ngs.ru>
2019-04-29 13:28:32 +0700

    refactoring

--- discovery.go
+++ discovery.go
@@ -7,9 +7,8 @@ import (
 	sentry "github.com/atlassian/go-sentry-api"
 )
 
-func discoveryOrgsProjects(
+func discoveryOrganizations(
 	organizations []sentry.Organization,
-	projects []sentry.Project,
 ) error {
 	discoveryData := make(map[string][]map[string]string)
 	var discoveredItems []map[string]string
@@ -17,15 +16,6 @@ func discoveryOrgsProjects(
 	for _, organization := range organizations {
 		discoveredItem := make(map[string]string)
 		discoveredItem["{#ORGANIZATION}"] = organization.Name
-		discoveredItem["{#TYPE}"] = "organization"
-		discoveredItems = append(discoveredItems, discoveredItem)
-	}
-
-	for _, project := range projects {
-		discoveredItem := make(map[string]string)
-		discoveredItem["{#PROJECT}"] = project.Name
-		discoveredItem["{#ORGANIZATION}"] = project.Organization.Name
-		discoveredItem["{#TYPE}"] = "project"
 		discoveredItems = append(discoveredItems, discoveredItem)
 	}
 
@@ -40,18 +30,16 @@ func discoveryOrgsProjects(
 	return nil
 }
 
-func discoveryQueue() error {
+func discoveryProjects(
+	projects []sentry.Project,
+) error {
 	discoveryData := make(map[string][]map[string]string)
 	var discoveredItems []map[string]string
 
-	queueNames, err := getQueueEvent()
-	if err != nil {
-		return err
-	}
-
-	for _, queueName := range queueNames {
+	for _, project := range projects {
 		discoveredItem := make(map[string]string)
-		discoveredItem["{#QUEUE}"] = queueName["queue"]
+		discoveredItem["{#PROJECT}"] = project.Name
+		discoveredItem["{#ORGANIZATION}"] = project.Organization.Name
 		discoveredItems = append(discoveredItems, discoveredItem)
 	}
 
--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
+	sentry "github.com/atlassian/go-sentry-api"
+	zsend "github.com/blacked/go-zabbix"
 	docopt "github.com/docopt/docopt-go"
 	hierr "github.com/reconquest/hierr-go"
 )
@@ -23,15 +26,15 @@ Usage:
   zabbix-agent-extension-sentry [options]
 
 Options:
-  --event                     Web event check.
-  --queue                     Cli queue check.
-
-Common options:
   -z --zabbix <zabbix>        Hostname or IP address of zabbix server
                                   [default: 127.0.0.1].
   -p --port <port>            Port of zabbix server [default: 10051].
   --zabbix-prefix <prefix>    Add part of your prefix for key [default: None].
   -d --discovery              Run low-level discovery for determine disks.
+
+Discovery options:
+  --organizations             Discovery organization.
+  --projects                  Discovery projects.
 
 Event options:
   -s --sentry <dsn>           Sentry DSN [default: http://localhost].
@@ -41,6 +44,13 @@ Event options:
 Other:
   -h --help                   Show this screen.
 `
+
+	var statTypes = []sentry.StatQuery{
+		"received",
+		"rejected",
+		"blacklisted",
+	}
+
 	args, err := docopt.Parse(usage, nil, true, version, false)
 	if err != nil {
 		fmt.Println(hierr.Errorf(err, "can't parse docopt"))
@@ -61,54 +71,124 @@ Other:
 		zabbixPrefix = fmt.Sprintf("%s.%s", zabbixPrefix, "sentry")
 	}
 
-	if args["--discovery"].(bool) {
-		discovery = true
-	}
-
 	hostname, err := os.Hostname()
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	if args["--event"].(bool) {
-		sentryDsn := args["--sentry"].(string)
-		sentryEpn := args["--endpoint"].(string)
-		sentryToken, ok := args["--token"].(string)
-		if !ok {
-			fmt.Println("need setup --token <tkn>")
-			os.Exit(1)
-		}
 
-		sentryApi := fmt.Sprintf("%s%s", sentryDsn, sentryEpn)
-
-		err = event(
-			sentryApi,
-			sentryToken,
-			discovery,
-			hostname,
-			zabbix,
-			port,
-			zabbixPrefix,
-		)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	sentryDsn := args["--sentry"].(string)
+	sentryEpn := args["--endpoint"].(string)
+	sentryToken, ok := args["--token"].(string)
+	if !ok {
+		fmt.Println("need setup --token <tkn>")
+		os.Exit(1)
 	}
 
-	if args["--queue"].(bool) {
-		err = queue(
-			discovery,
-			hostname,
-			zabbix,
-			port,
-			zabbixPrefix,
+	sentryApi := fmt.Sprintf("%s%s", sentryDsn, sentryEpn)
+
+	client, err := sentry.NewClient(sentryToken, &sentryApi, nil)
+	if err != nil {
+		fmt.Println(
+			hierr.Errorf(
+				err,
+				"can't connect sentry %s with token %s.",
+				sentryApi,
+				sentryToken,
+			),
 		)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+	}
+
+	organizations, _, err := client.GetOrganizations()
+	if err != nil {
+		fmt.Println(hierr.Errorf(err, "can't fetch organizations"))
+		os.Exit(1)
+	}
+
+	projects, err := client.GetProjects()
+	if err != nil {
+		fmt.Println(hierr.Errorf(err, "can't fetch projects"))
+		os.Exit(1)
+	}
+
+	if args["--discovery"].(bool) {
+		if args["--organizations"].(bool) {
+			if err := discoveryOrganizations(organizations); err != nil {
+				fmt.Println(hierr.Errorf(err, "can't discovery organizations"))
+			}
+		}
+		if args["--projects"].(bool) {
+			if err := discoveryProjects(projects); err != nil {
+				fmt.Println(hierr.Errorf(err, "can't discovery projects"))
+			}
+		}
+		os.Exit(0)
+	}
+
+	var metrics []*zsend.Metric
+
+	// 59 seconds interval, 6 time series value retrive
+	now := (time.Now().Unix()/10)*10 - 10
+	preview := time.Duration(59) * time.Second
+	later := now - int64(preview.Seconds())
+
+	for _, statType := range statTypes {
+
+		for _, organization := range organizations {
+
+			organizationStats, err := client.GetOrganizationStats(
+				organization,
+				statType,
+				later,
+				now,
+				nil)
+			if err != nil {
+				fmt.Println(
+					hierr.Errorf(err, "can't get organization stats"),
+				)
+			}
+
+			metrics = createOrganizationMetrics(
+				hostname,
+				organization.Name,
+				statType,
+				metrics,
+				organizationStats,
+				zabbixPrefix,
+			)
+
+			for _, project := range projects {
+				projectStats, err := client.GetProjectStats(
+					organization,
+					project,
+					statType,
+					later,
+					now,
+					nil)
+				if err != nil {
+					fmt.Println(
+						hierr.Errorf(err, "can't get project stats"),
+					)
+				}
+
+				metrics = createProjectMetrics(
+					hostname,
+					project.Organization.Name,
+					project.Name,
+					statType,
+					metrics,
+					projectStats,
+					zabbixPrefix,
+				)
+			}
 		}
 	}
+	packet := zsend.NewPacket(metrics)
+	sender := zsend.NewSender(
+		zabbix,
+		port,
+	)
+	sender.Send(packet)
 
 	fmt.Println("OK")
 }
